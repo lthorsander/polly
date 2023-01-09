eed6ee73af2fc5cc91a223638cfa5b41666136ff
MARR00M <m.romedahl@gmail.com>
2023-01-09 16:35:36 +0100

    emoji drawView

--- src/components/drawComp.vue
+++ src/components/drawComp.vue
@@ -51,7 +51,7 @@
 //import router from '@/router';
 export default {
     name: "drawComp",
-    props: ['timer', 'word', 'uiLabels', 'gameSocket', 'gameID'],
+    props: ['timer', 'word', 'uiLabels', 'gameSocket', 'gameID', 'choosenEmoji'],
     data() {
         return {
             canvas: null,
